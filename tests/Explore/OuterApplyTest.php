8b3fd4abb86d0c8609a725446425c1ffcb8f3f1b
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-11-11 20:59:24 -0600

    Put together an example of outer apply usage.

--- tests/Explore/OuterApplyTest.php
+++ tests/Explore/OuterApplyTest.php
@@ -8,38 +8,71 @@ class OuterApplyTest extends TestCase {
   public function setUp() {
     parent::setUp();
 
-    DB::unprepared('CREATE DATABASE Library;');
+    DB::unprepared('CREATE DATABASE bookstore;');
     $sql = <<<sql
-USE Library;
+USE bookstore;
 
-CREATE TABLE Authors (
+CREATE TABLE users (
     id int NOT NULL,
-    editor_id INT NULL,
-    author_name NVARCHAR(25) NOT NULL,
-    created_at DATETIME
-    CONSTRAINT PK_author_id PRIMARY KEY(id)
+    name NVARCHAR(128)
 );
+
+INSERT INTO users VALUES(1, 'Gwen');
+
+CREATE TABLE point_cards (
+   id int NOT NULL,
+   user_id int NOT NULL,
+   initial_point_balance int
+);
+INSERT INTO point_cards VALUES(1, 1, 100);
+INSERT INTO point_cards VALUES(1, 2, 100);
+
+CREATE TABLE purchases (
+  id int NOT NULL,
+  point_card_id int,
+  points_spent int
+);
+INSERT INTO purchases VALUES(1, 1, 10);
+INSERT INTO purchases VALUES(2, 1, 10);
+INSERT INTO purchases VALUES(3, 2, 20);
 sql;
-    
     DB::unprepared($sql);
   }
 
-  public function testOuterApply() {
-
+  /**
+   * Outer Apply creates a "table valued expression", an in memory table, for each left input, in the example below that
+   * means each row returned from "point_cards". This means that you can join multiple rows to a single row in the source
+   * table or perform an aggregate calculation, as below. The other awesomeness of Apply is that it is run before "select"
+   * which means you make up columns, as below, and use them in the list of columns selected from.
+   */
+  public function testOuterApplyByCalculatingASumInARelatedTable() {
+    
     $sql = <<<sql
-    SELECT * FROM Authors;
+    SELECT 
+      id, 
+      point_balance = (initial_point_balance - sp.total_points_spent)
+    FROM point_cards pc
+    OUTER APPLY (SELECT SUM(points_spent) total_points_spent FROM purchases p WHERE pc.id = p.point_card_id) sp
+    WHERE user_id = 1;
 sql;
 
-    $result = DB::select($sql);
-    $this->assertTrue(true);
+    $rows = DB::select($sql);
+    $this->assertEquals(
+      [
+        (object)[
+          'id' => 1,
+          'point_balance' => 80
+        ]
+      ],
+      $rows
+    );
   }
   
   public function tearDown() {
     $sql = <<<sql
 use master;
-DROP DATABASE Library;
+DROP DATABASE bookstore;
 sql;
-    
     DB::unprepared($sql);
     
     parent::tearDown();
