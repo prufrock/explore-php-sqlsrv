7e853fbd04c12d911f8fa76a17531c88a9ee6935
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-11-12 19:29:12 -0600

    Add examples of union and union all.

---
+++ tests/Explore/UnionTest.php
@@ -0,0 +1,82 @@
+<?php namespace Tests\Explore;
+
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class UnionTest extends TestCase {
+  
+  public function setUp() {
+    
+    parent::setUp();
+    
+    DB::unprepared('CREATE DATABASE bookshop');
+    
+    $sql =<<<sql
+use bookshop;
+CREATE TABLE new_books(
+  name NVARCHAR(128)
+);
+INSERT INTO new_books VALUES('The Rook');
+INSERT INTO new_books VALUES('Ready Player One');
+
+CREATE TABLE used_books(
+  name NVARCHAR(128)
+);
+INSERT INTO used_books VALUES('Patternmaster');
+INSERT INTO used_books VALUES('Ready Player One');
+sql;
+
+    DB::unprepared($sql);
+  }
+  
+  public function testUnion() {
+    
+    $sql =<<<sql
+SELECT * FROM new_books
+UNION
+SELECT * FROM used_books;
+sql;
+
+    
+    $rows = DB::select($sql);
+    
+    // The order shouldn't matter here but subset check doesn't seem to work as a set operation.
+    $this->assertArraySubset([
+      (object)['name' => 'Patternmaster'],
+      (object)['name' => 'Ready Player One'],
+      (object)['name' => 'The Rook']
+    ], 
+      $rows
+    );
+  }
+
+  public function testUnionAll() {
+
+    $sql =<<<sql
+SELECT * FROM new_books
+UNION ALL
+SELECT * FROM used_books;
+sql;
+
+
+    $rows = DB::select($sql);
+
+    // The order shouldn't matter here but subset check doesn't seem to work as a set operation.
+    $this->assertArraySubset([
+      (object)['name' => 'The Rook'],
+      (object)['name' => 'Ready Player One'],
+      (object)['name' => 'Patternmaster'],
+      (object)['name' => 'Ready Player One'],
+    ],
+      $rows
+    );
+  }
+  
+  public function tearDown() {
+
+    DB::unprepared('use master; DROP DATABASE bookshop;');
+    
+    parent::tearDown();
+  }
+
+}
