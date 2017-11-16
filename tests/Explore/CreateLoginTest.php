bb96c5f8cf7deb9f39dde4b4024618538d9d5ce5
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-11-16 15:36:19 -0600

    Try out creating a user.

---
+++ tests/Explore/CreateLoginTest.php
@@ -0,0 +1,28 @@
+<?php namespace Tests\Explore;
+
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class CreateLoginTest extends TestCase {
+
+  public function testCreateLogin() {
+
+    $sql =<<<sql
+CREATE LOGIN mary WITH PASSWORD = 'supersecret1!';
+CREATE USER MaryContrary FOR LOGIN mary;
+sql;
+
+    $result = DB::statement($sql);
+    $this->assertTrue($result);
+  }
+
+  public function tearDown() {
+
+    $sql =<<<sql
+DROP LOGIN mary;
+DROP USER MaryContrary;
+sql;
+
+    DB::statement($sql);
+  }
+}
