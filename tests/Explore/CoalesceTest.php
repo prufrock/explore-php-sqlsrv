e16dda02e2f1b1c2220a95a9d9be4bdee0ab02c1
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-12-26 10:46:03 -0600

    Playing with coalesce.

---
+++ tests/Explore/CoalesceTest.php
@@ -0,0 +1,49 @@
+<?php namespace Tests\Explore;
+
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+
+class CoalesceTest extends TestCase
+{
+
+    public function testCoalesceTwoValuesWithNullFirst()
+    {
+        $sql =<<<SQL
+SELECT COALESCE(NULL, 'Peter') name;
+SQL;
+        $rows = DB::select($sql);
+
+        $this->assertEquals('Peter', $rows[0]->name);
+    }
+
+    public function testCoalesceTwoValuesWithValueFirst()
+    {
+        $sql =<<<SQL
+SELECT COALESCE('June', NULL) name;
+SQL;
+        $rows = DB::select($sql);
+
+        $this->assertEquals('June', $rows[0]->name);
+    }
+
+    public function testCoalesceEvaluatesLeftToRightTakingTheFirstNotNullValue()
+    {
+        $sql =<<<SQL
+SELECT COALESCE(NULL, 'Franz', 'Hector', NULL) name;
+SQL;
+        $rows = DB::select($sql);
+
+        $this->assertEquals('Franz', $rows[0]->name);
+    }
+    
+    public function testCoalesceOnlyEvaluatesExpressionsThatAreNotNull()
+    {
+        $sql =<<<SQL
+SELECT COALESCE(NULL + 2, 3 * 4) name;
+SQL;
+        $rows = DB::select($sql);
+
+        $this->assertEquals(12, $rows[0]->name); 
+    }
+}
